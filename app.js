c882de8649fd1625c930726cead9186c9b5d7de1
Kane <ori.lowen@gmail.com>
2023-05-13 14:24:10 -0600

    Work on clear canvas button and resizing grid. Messed around but no real progress

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const container = document.getElementById("container");
+const container = document.getElementById('container');
 
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
+
+
   for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
+    let cell = document.createElement('div');
     cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
+    cell.addEventListener('mouseover', e => e.target.classList.add('my-color-class'));
   };
 };
 
-makeRows(16, 16);
+
+const clearBtn = document.getElementById('clear');
+clearBtn.addEventListener('click', () => {
+    .classList.add('clear-canvas-class'); 
+});
+
+
+makeRows(7, 7);
