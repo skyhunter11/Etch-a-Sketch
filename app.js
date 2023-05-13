18c790d725845886e7aad611b72e3b13f839eda8
Kane <ori.lowen@gmail.com>
2023-05-13 17:36:40 -0600

    Add resize button with limited functionality. Still need to figure out how to clear current contianer before updating container to new grid-size

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const container = document.getElementById('container');
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
-
-
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement('div');
     cell.innerText = (c + 1);
@@ -13,11 +11,13 @@ function makeRows(rows, cols) {
   };
 };
 
+//START HERE. Need to figure out how to clear out existing grid so new grid size can be drawn correctly. PERHAPS CLEAR the container somehow??
+const resizeBtn = document.getElementById('resize');
+resizeBtn.addEventListener('click', () => {
+    let newSizeInt = Number(prompt('Enter new grid size'));
 
-const clearBtn = document.getElementById('clear');
-clearBtn.addEventListener('click', () => {
-    .classList.add('clear-canvas-class'); 
-});
+    makeRows(0,0);
+    });
 
 
-makeRows(7, 7);
+makeRows(10, 10);
