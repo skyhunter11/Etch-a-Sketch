abcdae6262970027e77a77f2c7e2e0d846a911ba
Kane <ori.lowen@gmail.com>
2023-05-13 21:22:23 -0600

    Add function that clears grid and then draws new grid based on prompt input

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 const container = document.getElementById('container');
-
+// draw grid
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement('div');
     cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
+    container.appendChild(cell).className = 'grid-item';
     cell.addEventListener('mouseover', e => e.target.classList.add('my-color-class'));
   };
 };
 
-//START HERE. Need to figure out how to clear out existing grid so new grid size can be drawn correctly. PERHAPS CLEAR the container somehow??
+// label resize button
 const resizeBtn = document.getElementById('resize');
-resizeBtn.addEventListener('click', () => {
-    let newSizeInt = Number(prompt('Enter new grid size'));
 
-    makeRows(0,0);
+//clears old grid
+resizeBtn.addEventListener('click', () => {
+    const squares = document.querySelectorAll('.grid-item');
+     for (i = 0; i < squares.length; i++) {
+            squares[i].remove();
+     }
+// draws new grid from prompt input
+    let newSizeInt = Number(prompt('Enter new grid size'));
+    container.style.setProperty('grid-rows', newSizeInt);
+    container.style.setProperty('grid-cols', newSizeInt);
+    for (c = 0; c < (newSizeInt * newSizeInt); c++) {
+        let cell = document.createElement('div');
+        cell.innerText = (c + 1);
+        container.appendChild(cell).className = 'grid-item';
+        cell.addEventListener('mouseover', e => e.target.classList.add('my-color-class'));
+      };
     });
 
 
