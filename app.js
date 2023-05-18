abb4e6ae3120cfcbb30b92323c48fd74e52160de
Kane <ori.lowen@gmail.com>
2023-05-18 10:04:48 -0600

    Rework clearGrd and resizeGrid into their own functions

--- app.js
+++ app.js
@@ -1,55 +1,56 @@
-const container = document.getElementById('container');
-// draw grid
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement('div');
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = 'grid-item';
-    cell.addEventListener('mouseover', e => e.target.classList.add('my-color-class'));
-  };
-};
+// Etch-A-Sketch //
 
+// Declare container and draw grid
+const container = document.getElementById('container');
+drawGrid(10, 10);
 
 // label clear button
 const clearBtn = document.getElementById('clear');
-// sets all cells backrounds to white
-clearBtn.addEventListener('click', () => {
-    const cells = document.querySelectorAll('.grid-item');
-    for (i = 0; i < cells.length; i++){
-        cells[i].classList.remove('my-color-class');
-    }
-});
-
-// let allCells = document.querySelectorAll(".gamecell").forEach(cell => {
-//     cell.style.backgroundColor = "white";
-
+clearBtn.addEventListener('click', clearGrid);
 
 // label resize button
 const resizeBtn = document.getElementById('resize');
-//clears old grid
-resizeBtn.addEventListener('click', () => {
+resizeBtn.addEventListener('click', resizeGrid);
+
+
+// Functions //
+function drawGrid(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+      let cell = document.createElement('div');
+      cell.innerText = (c + 1);
+      container.appendChild(cell).className = 'grid-item';
+      // add color class when hovered over
+      cell.addEventListener('mouseover', e => e.target.classList.add('my-color-class'));
+    };
+  };
+
+function clearGrid() {
     const cells = document.querySelectorAll('.grid-item');
-     for (i = 0; i < cells.length; i++) {
-            cells[i].remove();
-     }
+    for (i = 0; i < cells.length; i++){
+        cells[i].classList.remove('my-color-class');
+    };
+};
+
+function resizeGrid() {
+    const cells = document.querySelectorAll('.grid-item');
+    for (i = 0; i < cells.length; i++) {
+           cells[i].remove();
+    };
 // draws new grid from prompt input
-    let newSizeInt = Number(prompt('Enter new grid size'));
-    container.style.setProperty('grid-rows', newSizeInt);
-    container.style.setProperty('grid-cols', newSizeInt);
-    for (c = 0; c < (newSizeInt * newSizeInt); c++) {
-        let cell = document.createElement('div');
-        cell.innerText = (c + 1);
-
-        cell.style.height = (700/newSizeInt);
-        cell.style.width = (700/newSizeInt);
-        //cell.classList.add('resize-class');
-
-        container.appendChild(cell).className = 'grid-item';
-        cell.addEventListener('mouseover', e => e.target.classList.add('my-color-class'));
-      };
-    });
-
-
-makeRows(10, 10);
+   let newSizeInt = Number(prompt('Enter new grid size'));
+   container.style.setProperty('grid-rows', newSizeInt);
+   container.style.setProperty('grid-cols', newSizeInt);
+   //
+   for (c = 0; c < (newSizeInt * newSizeInt); c++) {
+       let cell = document.createElement('div');
+       cell.innerText = (c + 1);
+       cell.style.height = (700/newSizeInt);
+       cell.style.width = (700/newSizeInt);
+       //cell.classList.add('resize-class');
+       container.appendChild(cell).className = 'grid-item';
+       // add color class when hovered over
+       cell.addEventListener('mouseover', e => e.target.classList.add('my-color-class'));
+     };
+};
